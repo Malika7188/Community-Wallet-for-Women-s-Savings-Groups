6c78160c14c889abcd376361a8051a4a43b7cbbd
Malika7188 <asmanodeny@gmail.com>
2025-07-18 18:35:56 +0300

    added routes

---
+++ chama-wallet-backend/routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"chama-wallet-backend/handlers"
+)
+
+func Setup(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("🚀 Community Wallet API is running.")
+	})
+	app.Post("/create-wallet", handlers.CreateWallet)
+	app.Get("/balance/:address", handlers.GetBalance)
+	app.Post("/transfer", handlers.TransferFunds)
+	app.Get("/generate-keypair", handlers.GenerateKeypair)
+	app.Post("/fund/:address", handlers.FundAccount)
+	app.Get("/transactions/:address", handlers.GetTransactionHistory)
+	
+
+
+}
