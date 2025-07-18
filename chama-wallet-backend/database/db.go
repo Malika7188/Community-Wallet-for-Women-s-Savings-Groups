34601193583681051bf665cdafc77474d42661ca
Malika7188 <asmanodeny@gmail.com>
2025-07-19 02:30:45 +0300

    added db

---
+++ chama-wallet-backend/database/db.go
@@ -0,0 +1,32 @@
+// database/db.go
+package database
+
+import (
+	"fmt"
+	"log"
+	// "your_project_name/smodels" // replace with your actual module
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"chama-wallet-backend/models"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := "host=localhost user=chama_user password=malika dbname=chama_wallet port=5432 sslmode=disable"
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to database:", err)
+	}
+
+	err = DB.AutoMigrate(&models.Group{}, &models.Member{}, &models.Contribution{})
+	if err != nil {
+		log.Fatal("failed to migrate database:", err)
+	}
+
+	fmt.Println("Connected to the database successfully.")
+}
