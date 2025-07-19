330e71a60741d1d2e64ab4717211950c31b013b7
Malika7188 <asmanodeny@gmail.com>
2025-07-19 15:29:28 +0300

    updated route for group creation

--- chama-wallet-backend/routes/group.go
+++ chama-wallet-backend/routes/group.go
@@ -8,12 +8,14 @@ import (
 )
 
 func GroupRoutes(app *fiber.App) {
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
 
 	app.Post("/group/create", handlers.CreateGroup)
 	app.Post("/group/:id/contribute", handlers.ContributeToGroup)
 	app.Post("/group/:id/join", handlers.AddMember)
 	app.Get("/group/:id/balance", handlers.GetGroupBalance)
 	app.Get("/groups", handlers.GetAllGroups)
-
 
 }
