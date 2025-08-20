946120a8a8aede49dd9d31bdc3ce20985fb16223
Malika7188 <asmanodeny@gmail.com>
2025-08-21 02:06:59 +0300

    added a backend api. for delete functionality

--- chama-wallet-backend/routes/routes.go
+++ chama-wallet-backend/routes/routes.go
@@ -11,14 +11,15 @@ func Setup(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("🚀 Community Wallet API is running.")
 	})
-	
+
 	// Public wallet routes
 	app.Post("/create-wallet", handlers.CreateWallet)
 	app.Get("/balance/:address", middleware.OptionalAuthMiddleware(), handlers.GetBalance)
 	app.Get("/generate-keypair", handlers.GenerateKeypair)
 	app.Post("/fund/:address", middleware.OptionalAuthMiddleware(), handlers.FundAccount)
 	app.Get("/transactions/:address", middleware.OptionalAuthMiddleware(), handlers.GetTransactionHistory)
-	
+	app.Get("/deleteNotification", middleware.AuthMiddleware(), handlers.DeleteNotification)
+
 	// Protected wallet routes
 	app.Post("/transfer", middleware.AuthMiddleware(), handlers.TransferFunds)
 	app.Post("/api/contribute", handlers.ContributeHandler)
